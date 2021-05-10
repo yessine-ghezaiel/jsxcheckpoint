18eea251bd272bcd7424439e87ec1e152a6fa7a8
yessine-ghezaiel <yessineghezaiel@gmail.com>
2021-05-10 15:02:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import img from './killua.jpg';
 import './App.css';
+import './style.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="awesome" style={{border: '1px solid red'}}>
+          <label htmlFor="name">Enter your name: </label>
+          <input type="text" id="name" />
+          <h1 className="title red">Your name here</h1>
+          <br />
+          <img src={img} />
+          <br />
+          <img src="./luffy.jpg" />
+        </div>
+        <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+
     </div>
   );
 }
